96fd1c5626b9ab9b48318f58bf76382cf229e1ed
Uczen <Uczen@DESKTOP-0MIMCUJ>
2023-09-28 08:56:55 +0200

    Dodaj pliki projektów.

---
+++ calculator/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+            
+
+        private void button1_Clicked(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ calculator/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace calculator
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
